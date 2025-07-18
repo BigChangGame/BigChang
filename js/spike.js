61839f2102a5929768c200764eb37c76be8418a2
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-07-18 13:02:27 +0700

    Update spike.js

--- js/spike.js
+++ js/spike.js
@@ -1,13 +1,13 @@
 const spikedata = [
   
   {
-          "title": "PUBG MOBILE VNG V3",
-              "version": "3.8",
+          "title": "PUBG MOBILE VNG V2",
+              "version": "3.9",
               "image": "https://i.imgur.com/LTMCdc7.jpeg",
               "image-demo": "https://i.imgur.com/u2YoDSe.jpeg",
-              "description": "✅MENU FULL CHỨC NĂNG! \n ✅ESP An toàn! \n ✅Aimbot \n ✅ModSkin \n ✅Nhiều chức năng khác! \n ✅UNLOCK SKIN  \n ✅Nhiều chức năng khác!",
-              "download_url": "https://yeumoney.com/Yw9FYA3",
-              "updated_at": "2025-06-5",
+              "description": "✅MENU FULL CHỨC NĂNG! \n ✅ESP An toàn! \n ✅Aimbot \n ✅ModSkin \n ✅Nhiều chức năng khác! \n ✅Nhiều chức năng khác! \n ✅ANTIBAN",
+              "download_url": "https://yeumoney.com/Tg-3sMD",
+              "updated_at": "2025-07-17",
               "size": "3.6 GB",
               "fake_downloads": 9999999
   },
