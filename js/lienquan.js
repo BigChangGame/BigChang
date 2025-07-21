8fc184f76f44f211d2d37ea6dc0dc4f149f2f0b9
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-07-21 09:14:58 +0700

    Update lienquan.js

--- js/lienquan.js
+++ js/lienquan.js
@@ -16,7 +16,7 @@ const lienquandata = [
                 "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/f399da84-52b5-4dfc-82bb-b3a99ce8ce72.image.png?v=1681997379317%27",
                 "image-demo": "https://i.imgur.com/1H6OK0J.jpeg",
                 "description": "✅Menu Full chức năng \n ✅ESP / Ẩn Hack \n ✅AUTO BỘC PHÁ, BĂNG SƯƠNG, TRỪNG TR…  \n ✅AIM Elsu \n ✅MACRO FLO \n ✅Muốn bán đồ nhanh  \n ✅Cam xa tuỳ chỉnh + Đổi Tên Dài.  \n ✅Nhiều chức năng khác…\n ✅Fix ban 7day + Fix Ban 3-10 năm.",
-                "download_url": "https://yeumoney.com/FsX2GeVy_s",
+                "download_url": "https://yeumoney.com/OWCtd",
                 "updated_at": "2025-07-22",
                 "size": "340.6 MB",
                 "fake_downloads": 9999999
