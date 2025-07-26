1b29cd3da052864305bc4206d6ecbeabe6bc32b4
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-07-27 00:26:09 +0700

    Update lienquan.js

--- js/lienquan.js
+++ js/lienquan.js
@@ -27,8 +27,8 @@ const lienquandata = [
                 "image": "https://cdn.glitch.global/a5da2f18-878d-4722-b1ad-5ae2f04638f8/f399da84-52b5-4dfc-82bb-b3a99ce8ce72.image.png?v=1681997379317%27",
                 "image-demo": "https://i.imgur.com/kLVZpFq.png",
                 "description": "⭐️Bản Leo Rank Chấp Tố Nhẹ ! \n ✅Vip Antiban\n ✅Ẩn Hack Live Strem \n ✅AIM Elsu \n ✅Chat X2 \n ✅Cam xa tuỳ chỉnh + Đổi Tên Dài.  \n ✅UNLOCK SKIN\n ✅Huỷ trận không mất sao\n✅Full Chức Năng \n✅Fix ban 7day + Fix Ban 3-10 năm.",
-                "download_url": "https://yeumoney.com/UsxsJov",
-                "updated_at": "2025-05-22",
+                "download_url": "https://yeumoney.com/0B9J3DBkt",
+                "updated_at": "2025-07-22",
                 "size": "340.6 MB",
                 "fake_downloads": 9999999
     }
