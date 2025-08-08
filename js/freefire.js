39d142ce604670ec8d865cc9274c069d95e155fb
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-08-08 14:19:26 +0700

    Update freefire.js

--- js/freefire.js
+++ js/freefire.js
@@ -3,7 +3,7 @@ const freefiredata = [
         "title": "FreeFire Menu",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/00/f0/88/00f08867-9cfc-7718-fb66-b829298dc7ea/AppIcon-1747240258-1x_U007emarketing-0-8-0-85-220-0.png/434x0w.webp",
         "image-demo": "https://i.imgur.com/o21QUcT.jpeg",
-        "description": "✅Deb Menu Không Key",
+        "description": "✅LƯU Ý: Không Bật Aim để tránh văng Game✅ESP \n✅Aimbot \n✅Bypass Fix ban \n✅Fake Lag \n✅Antiban \n✅Antiban(Phòng) \n✅Antiban(Tử chiến) \n✅Antiban(Huấn Luyện)",
         "download_url": "https://yeumoney.com/wVmOzO",
         "version": "1.111.1",
         "updated_at": "2025-6-14",
