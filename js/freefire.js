a7c2f957fd9f136d568c83f9ff1610e67ee8f0c8
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-08-08 14:25:20 +0700

    Update freefire.js

--- js/freefire.js
+++ js/freefire.js
@@ -4,7 +4,7 @@ const freefiredata = [
         "image": "https://play-lh.googleusercontent.com/jHnf_3-yMox_Tub_oLKQ7OuOi8Iahf1f4rdu-etZ2hmzUOekY84znct-c8hksxhD15PYNk4dMH2ZdVY3uxm7Gw=w480-h960",
         "image-demo": "https://i.imgur.com/kDIryPc.jpeg",
         "description": "✅LƯU Ý: Không Bật Aim để tránh văng Game \n✅ESP \n✅No KEY \n✅Bypass Fix ban \n✅Fake Lag \n✅Antiban \n✅Antiban(Phòng) \n✅Antiban(Tử chiến) \n✅Antiban(Huấn Luyện)",
-        "download_url": "https://yeumoney.com/wVmOzO",
+        "download_url": "https://yeumoney.com/jiJ7rt",
         "version": "1.114.1",
         "updated_at": "2025-8-14",
         "size": "... MB "
