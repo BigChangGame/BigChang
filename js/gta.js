2bdb113a6d583d74488be0a398e7de9543ff4a4c
BigChang <119626114+BigChangGame@users.noreply.github.com>
2025-08-16 00:18:12 +0700

    Update gta.js

--- js/gta.js
+++ js/gta.js
@@ -1,12 +1,12 @@
 const gtadata = [
  {
         "title": "Call of Duty VN - NOKEY",
-        "version": "1.8.51",
+        "version": "1.8.52",
         "image": "https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/08/9b/5d/089b5d63-af81-cde6-2a06-f0760740fa96/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/434x0w.webp",
         "image-demo": "https://i.imgur.com/ZkqeYlF.png",
         "description": "✅Menu Full chức Năng không KEY \n✅ESP \n✅Chạy Nhanh\n✅ Aimbot \n✅ Antiban 90% \n✅ Fix Văng ",
         "download_url": "https://yeumoney.com/d7GsGX",
-        "updated_at": "2025-07-05",
+        "updated_at": "2025-08-15",
         "size": "2.5 GB",
  },
  {
@@ -343,6 +343,7 @@ const gtadata = [
         "size": "1.85 GB"
     }
 ];
+
 
 
 
